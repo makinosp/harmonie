21ebd9010971122c7bc11e1a474f3ee130b0c244
makinosp <makinosp@icloud.com>
2024-03-16 22:00:50 +0900

    feat: tab view template

---
+++ friendplus/Views/FavoritesView.swift
@@ -0,0 +1,14 @@
+//
+//  FavoritesView.swift
+//  friendplus
+//
+//  Created by makinosp on 2024/03/16.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text("FavoritesView")
+    }
+}
---
+++ friendplus/Views/LocationsView.swift
@@ -0,0 +1,14 @@
+//
+//  LocationsView.swift
+//  friendplus
+//
+//  Created by makinosp on 2024/03/16.
+//
+
+import SwiftUI
+
+struct LocationsView: View {
+    var body: some View {
+        Text("LocationsView")
+    }
+}
