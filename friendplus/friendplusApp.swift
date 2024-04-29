63a1780979a71e9cc7c39759a8c0dc80c1a099ad
makinosp <makinosp@icloud.com>
2024-04-29 13:15:01 +0900

    refact: unused code

--- friendplus/friendplusApp.swift
+++ friendplus/friendplusApp.swift
@@ -7,10 +7,6 @@
 
 import SwiftUI
 
-var isPreview: Bool {
-    ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
-}
-
 @main
 struct FriendplusApp: App {
     var body: some Scene {
