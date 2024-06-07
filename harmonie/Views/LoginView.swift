4a0a2d7aa65503f116210290fd188c28f76c6cf8
makinosp <makinosp@icloud.com>
2024-06-07 23:02:09 +0900

    fix: login

--- harmonie/Views/LoginView.swift
+++ harmonie/Views/LoginView.swift
@@ -10,7 +10,7 @@ import VRCKit
 
 struct LoginView: View {
     @EnvironmentObject var userData: UserData
-    @State var requiresTwoFactorAuth: [String] = []
+    @State var requiresTwoFactorAuth: [TwoFactorAuthType] = []
     @State var username: String = ""
     @State var password: String = ""
     @State var code: String = ""
@@ -19,89 +19,76 @@ struct LoginView: View {
     @State var isPresentedAlert = false
     @State var vrckError: VRCKitError? = nil
 
+    private typealias Service = AuthenticationService
+
     var body: some View {
-        ZStack {
-            Color(UIColor.systemGroupedBackground)
+        VStack(spacing: 16) {
             if requiresTwoFactorAuth.isEmpty {
                 usernamePasswordFields
+                loginButton("Login", login)
             } else {
                 otpField
+                loginButton("Continue", verifyTwoFA)
             }
         }
+        .padding(32)
+        .ignoresSafeArea(.keyboard)
         .alert(isPresented: $isPresentedAlert, error: vrckError) { _ in
             Button("OK") {}
         } message: { error in
             Text(error.failureReason ?? "Try again later.")
         }
-        .edgesIgnoringSafeArea(.all)
     }
 
     var usernamePasswordFields: some View {
-        VStack(spacing: 24) {
-            VStack(spacing: 8) {
-                HStack {
-                    Image(systemName: "at")
-                        .foregroundStyle(Color.gray)
-                    TextField("UserName", text: $username)
-                }
-                .padding(8)
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .foregroundStyle(Color(UIColor.systemBackground))
-                )
-                HStack {
-                    Image(systemName: "lock")
-                        .foregroundStyle(Color.gray)
-                    SecureField("Password", text: $password)
-                }
-                .padding(8)
-                .background(
-                    RoundedRectangle(cornerRadius: 8)
-                        .foregroundStyle(Color(UIColor.systemBackground))
-                )
+        VStack(spacing: 8) {
+            HStack {
+                Image(systemName: "at")
+                    .foregroundStyle(Color.gray)
+                TextField("UserName", text: $username)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
-            if isRunning {
-                ProgressView()
-            } else {
-                AsyncButton {
-                    await login()
-                } label: {
-                    Label("Login", systemImage: "chevron.right")
-                }
+            .padding(.horizontal, 8)
+            HStack {
+                Image(systemName: "lock")
+                    .foregroundStyle(Color.gray)
+                SecureField("Password", text: $password)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
+            .padding(.horizontal, 8)
         }
-        .padding(24)
     }
 
     var otpField: some View {
-        VStack(spacing: 24) {
-            HStack {
-                Image(systemName: "ellipsis")
-                    .foregroundStyle(Color.gray)
-                TextField("Code", text: $code)
-            }
-            .padding(8)
-            .background(
-                RoundedRectangle(cornerRadius: 8)
-                    .foregroundStyle(Color(UIColor.systemBackground))
-            )
-            if isRunning {
-                ProgressView()
-            } else {
-                AsyncButton {
-                    await verifyTwoFA()
-                } label: {
-                    Image(systemName: "arrow.right")
-                        .bold()
-                        .font(.title2)
-                        .frame(width: 48, height: 48)
-                        .foregroundColor(Color.white)
-                        .background(Color.blue)
-                        .clipShape(Circle())
-                }
-            }
+        HStack {
+            Image(systemName: "ellipsis")
+                .foregroundStyle(Color.gray)
+            TextField("Code", text: $code)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
         }
-        .padding(24)
+        .padding(.horizontal, 8)
+        .background(
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundStyle(Color(UIColor.systemBackground))
+        )
+    }
+
+    @ViewBuilder
+    func loginButton(
+        _ text: String,
+        _ action: @escaping () async -> Void
+    ) -> some View {
+        if isRunning {
+            ProgressView()
+        } else {
+            AsyncButton {
+                await action()
+            } label: {
+                Text(text)
+            }
+            .buttonStyle(.bordered)
+            .buttonBorderShape(.capsule)
+        }
     }
 
     func login() async {
@@ -110,8 +97,8 @@ struct LoginView: View {
             password: password
         )
         do {
-            switch try await AuthenticationService.loginUserInfo(userData.client) {
-            case let value as [String]:
+            switch try await Service.loginUserInfo(userData.client) {
+            case let value as [TwoFactorAuthType]:
                 requiresTwoFactorAuth = value
             case let value as User:
                 userData.user = value
@@ -130,23 +117,16 @@ struct LoginView: View {
     }
 
     func verifyTwoFA() async {
-        var verifyType: String?
-        if requiresTwoFactorAuth.contains(TwoFactorAuthType.totp.rawValue) {
-            verifyType = TwoFactorAuthType.totp.rawValue
-        } else if requiresTwoFactorAuth.contains(TwoFactorAuthType.emailotp.rawValue) {
-            verifyType = TwoFactorAuthType.emailotp.rawValue
+        guard let verifyType = Service.getVerifyType(requiresTwoFactorAuth) else {
+            return
         }
-        guard let verifyType = verifyType else { return }
         do {
-            if try await AuthenticationService.verify2FA(
+            if try await Service.verify2FA(
                 userData.client,
                 verifyType: verifyType,
                 code: code
             ) {
                 userData.step = .loggedIn
-            } else {
-                // TODO: throw error
-                print("not verified")
             }
         } catch let error as VRCKitError {
             isPresentedAlert = true
