cd231fb3be798fddeed11a05236d19bb9e114ad3
makinosp <makinosp@icloud.com>
2024-07-17 23:08:00 +0900

    feat: add file LocationDetailView

---
+++ harmonie/Views/Location/LocationDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  LocationDetailView.swift
+//  harmonie
+//
+//  Created by makinosp on 2024/07/17.
+//
+
+import AsyncSwiftUI
+import NukeUI
+import VRCKit
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text("LocationDetailView")
+    }
+}
