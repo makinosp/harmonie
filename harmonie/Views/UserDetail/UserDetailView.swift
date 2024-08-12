b1889ec0808451ec619c87e0766386a9efebf9b5
makinosp <makinosp@icloud.com>
2024-08-12 12:45:27 +0900

    refact: split the logic of activitySection of UserDetailView into a file

--- harmonie/Views/UserDetail/UserDetailView.swift
+++ harmonie/Views/UserDetail/UserDetailView.swift
@@ -246,39 +246,4 @@ struct UserDetailView: View {
             }
         }
     }
-
-    var lastLoginText: String {
-        [
-            DateUtil.shared.formatToyyyyMMdd(from: user.lastLogin),
-            DateUtil.shared.formatToHHmm(from: user.lastLogin)
-        ].joined(separator: " ")
-    }
-
-    var lastActivityText: String {
-        [
-            DateUtil.shared.formatToyyyyMMdd(from: user.lastActivity),
-            DateUtil.shared.formatToHHmm(from: user.lastActivity)
-        ].joined(separator: " ")
-    }
-
-    var activitySection: some View {
-        SectionView {
-            VStack(alignment: .leading, spacing: 8) {
-                VStack(alignment: .leading) {
-                    Text("Last Login")
-                        .font(.subheadline)
-                        .foregroundStyle(Color.gray)
-                    Text(lastLoginText)
-                        .font(.body)
-                }
-                VStack(alignment: .leading) {
-                    Text("Last Activity")
-                        .font(.subheadline)
-                        .foregroundStyle(Color.gray)
-                    Text(lastActivityText)
-                        .font(.body)
-                }
-            }
-        }
-    }
 }
