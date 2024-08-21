182f258d8b7d023846c20ff20d96601a2f1e6b33
makinosp <makinosp@icloud.com>
2024-08-21 21:39:49 +0900

    feat: implement bio editor in ProfileEditView form

--- harmonie/Views/Setting/ProfileEditView.swift
+++ harmonie/Views/Setting/ProfileEditView.swift
@@ -9,26 +9,37 @@ import SwiftUI
 import VRCKit
 
 struct ProfileEditView: View {
+    @Environment(\.dismiss) private var dismiss
     @State var status: UserStatus
+    @State var statusDescription: String
+    @State var bio: String
 
     init(user: User) {
         _status = State(initialValue: user.status)
+        _statusDescription = State(initialValue: user.statusDescription)
+        _bio = State(initialValue: user.bio ?? "")
     }
 
     var body: some View {
         NavigationStack {
             Form {
-                Picker(selection: $status) {
-                    ForEach(UserStatus.allCases) { status in
-                        Text(status.description).tag(status)
+                Section("Status") {
+                    Picker(selection: $status) {
+                        ForEach(UserStatus.allCases) { status in
+                            Text(status.description).tag(status)
+                        }
+                    } label: {
+                        Label {
+                            Text("Status")
+                        } icon: {
+                            Image(systemName: "circle.fill")
+                                .foregroundStyle(status.color)
+                        }
                     }
-                } label: {
-                    Label {
-                        Text("Status")
-                    } icon: {
-                        Image(systemName: "circle.fill")
-                            .foregroundStyle(status.color)
-                    }
+                    TextField("Status Description", text: $statusDescription)
+                }
+                Section("Description") {
+                    TextEditor(text: $bio)
                 }
             }
             .toolbar { toolbarContents }
@@ -38,7 +49,7 @@ struct ProfileEditView: View {
     @ToolbarContentBuilder var toolbarContents: some ToolbarContent {
         ToolbarItem(placement: .cancellationAction) {
             Button {
-                print("Saving!")
+                dismiss()
             } label: {
                 Text("Cancel")
             }
