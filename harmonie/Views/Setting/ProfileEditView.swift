5e8097df5c858825fe752309bd68435f1fa5ea1e
makinosp <makinosp@icloud.com>
2024-08-21 22:06:08 +0900

    feat: implement language tag picker in ProfileEditView

--- harmonie/Views/Setting/ProfileEditView.swift
+++ harmonie/Views/Setting/ProfileEditView.swift
@@ -13,11 +13,13 @@ struct ProfileEditView: View {
     @State var status: UserStatus
     @State var statusDescription: String
     @State var bio: String
+    @State var languageTags: [LanguageTag]
 
     init(user: User) {
         _status = State(initialValue: user.status)
         _statusDescription = State(initialValue: user.statusDescription)
         _bio = State(initialValue: user.bio ?? "")
+        _languageTags = State(initialValue: user.tags.languageTags)
     }
 
     var body: some View {
@@ -40,6 +42,21 @@ struct ProfileEditView: View {
                 }
                 Section("Description") {
                     TextEditor(text: $bio)
+                }
+                Section("Language") {
+                    ForEach(languageTags) { tag in
+                        Text(tag.rawValue)
+                            .swipeActions {
+                                Button(role: .destructive) {
+                                    // Delete action
+                                } label: {
+                                    Text("Delete")
+                                }
+                            }
+                    }
+                }
+                Button {} label: {
+                    Label("Add", systemImage: "plus")
                 }
             }
             .toolbar { toolbarContents }
