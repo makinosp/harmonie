61c2499cfd8aa44f9265f36eb48e6e889b3163a1
makinosp <makinosp@icloud.com>
2024-08-24 12:23:27 +0900

    feat: implement entering URL text field

--- harmonie/Views/Setting/URLEditorView.swift
+++ harmonie/Views/Setting/URLEditorView.swift
@@ -11,19 +11,34 @@ import VRCKit
 struct URLEditorView: View {
     @Environment(\.dismiss) private var dismiss
     @Binding private var inputtedURL: String
+    @State private var urlString: String
+    @State private var isInvalid = false
 
     init(inputtedURL: Binding<String>) {
         _inputtedURL = inputtedURL
+        _urlString = State(initialValue: inputtedURL.wrappedValue)
     }
 
     var body: some View {
         NavigationStack {
             Form {
-                TextField("https://", text: $inputtedURL)
+                TextField("https://", text: $urlString)
+                    .textInputAutocapitalization(.never)
+                    .onChange(of: urlString, initial: true) {
+                        isInvalid = !isValidURLFormat(urlString)
+                    }
             }
             .navigationTitle("Enter URL")
             .toolbarTitleDisplayMode(.inline)
             .toolbar { toolbarContents }
+        }
+    }
+
+    private func isValidURLFormat(_ string: String) -> Bool {
+        if let url = URL(string: string), UIApplication.shared.canOpenURL(url) {
+            return true
+        } else {
+            return false
         }
     }
 
@@ -37,10 +52,12 @@ struct URLEditorView: View {
         }
         ToolbarItem {
             Button {
+                inputtedURL = urlString
                 dismiss()
             } label: {
                 Text("Confirm")
             }
+            .disabled(isInvalid)
         }
     }
 }
