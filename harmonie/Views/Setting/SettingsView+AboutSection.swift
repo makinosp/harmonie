95a2a13f51ad56eb18822fe45d922135c6be59d7
makinosp <makinosp@icloud.com>
2024-08-24 14:47:39 +0900

    refact: cut out about section of SettingsView into a file

---
+++ harmonie/Views/Setting/SettingsView+AboutSection.swift
@@ -0,0 +1,48 @@
+//
+//  SettingsView+AboutSection.swift
+//  Harmonie
+//
+//  Created by makinosp on 2024/08/24.
+//
+
+import SwiftUI
+
+extension SettingsView {
+    var aboutSection: some View {
+        Section("About") {
+            LabeledContent {
+                Constants.Icon.forward
+            } label: {
+                Button {
+                    destination = .about
+                } label: {
+                    Label {
+                        Text("About This App")
+                    } icon: {
+                        Image(systemName: "info.circle")
+                    }
+                }
+            }
+            Link(destination: URL(string: "https://github.com/makinosp/harmonie")!) {
+                Label {
+                    Text("Source Code")
+                } icon: {
+                    Image(systemName: "curlybraces")
+                }
+            }
+            LabeledContent {
+                Constants.Icon.forward
+            } label: {
+                Button {
+                    destination = .license
+                } label: {
+                    Label {
+                        Text("Third Party Licence")
+                    } icon: {
+                        Image(systemName: "lightbulb")
+                    }
+                }
+            }
+        }
+    }
+}
--- harmonie/Views/Setting/SettingsView.swift
+++ harmonie/Views/Setting/SettingsView.swift
@@ -65,41 +65,7 @@ struct SettingsView: View {
             if let user = appVM.user {
                 profileSection(user: user)
             }
-            Section("About") {
-                LabeledContent {
-                    Constants.Icon.forward
-                } label: {
-                    Button {
-                        destination = .about
-                    } label: {
-                        Label {
-                            Text("About This App")
-                        } icon: {
-                            Image(systemName: "info.circle")
-                        }
-                    }
-                }
-                Link(destination: URL(string: "https://github.com/makinosp/harmonie")!) {
-                    Label {
-                        Text("Source Code")
-                    } icon: {
-                        Image(systemName: "curlybraces")
-                    }
-                }
-                LabeledContent {
-                    Constants.Icon.forward
-                } label: {
-                    Button {
-                        destination = .license
-                    } label: {
-                        Label {
-                            Text("Third Party Licence")
-                        } icon: {
-                            Image(systemName: "lightbulb")
-                        }
-                    }
-                }
-            }
+            aboutSection
             Section {
                 AsyncButton {
                     await appVM.logout()
