a81699187d655dea5fec0888703cdcde43786bd2
makinosp <makinosp@icloud.com>
2024-08-31 14:02:05 +0900

    refact: improvement UI of AuthenticationView

--- harmonie/Views/Authentication/AuthenticationView.swift
+++ harmonie/Views/Authentication/AuthenticationView.swift
@@ -16,28 +16,14 @@ struct AuthenticationView: View {
     @State private var password: String = ""
     @State private var code: String = ""
     @State private var isRequesting = false
+    @State private var isPresentedPopover = false
 
     var body: some View {
         VStack(spacing: 16) {
             if verifyType == nil {
-                usernamePasswordFields
-                Toggle(
-                    "Save in Keychain",
-                    systemImage: "key.icloud",
-                    isOn: $isSavedOnKeyChain
-                )
-                loginButton("Login") {
-                    verifyType = await appVM.login(
-                        username: username,
-                        password: password,
-                        isSavedOnKeyChain: isSavedOnKeyChain
-                    )
-                }
+                loginViewGroup
             } else {
-                otpField
-                loginButton("Continue") {
-                    await appVM.verifyTwoFA(verifyType, code)
-                }
+                otpViewGroup
             }
         }
         .padding(32)
@@ -50,39 +36,68 @@ struct AuthenticationView: View {
         }
     }
 
-    private var usernamePasswordFields: some View {
-        VStack(spacing: 8) {
-            HStack {
-                Image(systemName: "at")
-                    .foregroundStyle(Color.gray)
-                TextField("UserName", text: $username)
-                    .textInputAutocapitalization(.never)
-                    .textFieldStyle(.roundedBorder)
-            }
-            .padding(.horizontal, 8)
-            HStack {
-                Image(systemName: "lock")
-                    .foregroundStyle(Color.gray)
-                SecureField("Password", text: $password)
-                    .textFieldStyle(.roundedBorder)
-            }
-            .padding(.horizontal, 8)
+    @ViewBuilder private var loginViewGroup: some View {
+        loginFields
+        loginButton("Login") {
+            verifyType = await appVM.login(
+                username: username,
+                password: password,
+                isSavedOnKeyChain: isSavedOnKeyChain
+            )
         }
     }
 
-    private var otpField: some View {
-        HStack {
-            Image(systemName: "ellipsis")
-                .foregroundStyle(Color.gray)
-            TextField("Code", text: $code)
-                .keyboardType(.decimalPad)
+    @ViewBuilder private var otpViewGroup: some View {
+        otpField
+        loginButton("Continue") {
+            await appVM.verifyTwoFA(verifyType, code)
+        }
+    }
+
+    private var loginFields: some View {
+        VStack(spacing: 8) {
+            TextField("UserName", text: $username)
+                .textInputAutocapitalization(.never)
                 .textFieldStyle(.roundedBorder)
+            SecureField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+            Toggle(isOn: $isSavedOnKeyChain) {
+                HStack {
+                    Label {
+                        Text("Store in Keychain")
+                    } icon: {
+                        Image(systemName: "key.icloud")
+                    }
+                    Button {
+                        isPresentedPopover.toggle()
+                    } label: {
+                        Image(systemName: "questionmark.circle")
+                    }
+                    .popover(isPresented: $isPresentedPopover) {
+                        Text(helpText)
+                            .padding()
+                            .presentationDetents([.fraction(1/4)])
+                    }
+                }
+                .font(.callout)
+                .foregroundStyle(Color(uiColor: .systemGray))
+            }
         }
         .padding(.horizontal, 8)
-        .background(
-            RoundedRectangle(cornerRadius: 8)
-                .foregroundStyle(Color(uiColor: .systemBackground))
-        )
+    }
+
+    let helpText = """
+            Using iCloud Keychain to securely store your passwords. \
+            iCloud Keychain is built on security technologies provided by Apple, \
+            ensuring that your passwords are encrypted \
+            and protected from unauthorized access.
+            """
+
+    private var otpField: some View {
+        TextField("Code", text: $code)
+            .keyboardType(.decimalPad)
+            .textFieldStyle(.roundedBorder)
+            .padding(.horizontal, 8)
     }
 
     @ViewBuilder
@@ -106,4 +121,5 @@ struct AuthenticationView: View {
 
 #Preview {
     AuthenticationView()
+        .environment(AppViewModel())
 }
