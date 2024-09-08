1e75113b88c9e169f33bc3517552d74778acfa07
makinosp <makinosp@icloud.com>
2024-09-08 21:32:27 +0900

    fix: behavior of friend search

--- harmonie/Views/Friend/FriendsView.swift
+++ harmonie/Views/Friend/FriendsView.swift
@@ -12,6 +12,7 @@ struct FriendsView: View {
     @Environment(AppViewModel.self) var appVM: AppViewModel
     @Environment(FriendViewModel.self) var friendVM: FriendViewModel
     @Environment(FavoriteViewModel.self) var favoriteVM: FavoriteViewModel
+    @State private var searchText = ""
     @State var selected: Selected?
 
     var body: some View {
@@ -83,11 +84,14 @@ struct FriendsView: View {
             }
         }
         .navigationTitle("Friends")
-        .searchable(text: bindFilterText)
         .toolbar { toolbarContent }
         .navigationDestination(item: $selected) { selected in
             UserDetailPresentationView(id: selected.id)
                 .id(selected.id)
+        }
+        .searchable(text: $searchText)
+        .onSubmit(of: .search) {
+            friendVM.filterText = searchText
         }
     }
 }
