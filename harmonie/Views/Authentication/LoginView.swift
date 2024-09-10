694c9cbadf2084a00a9419c7e40c7a18c6a7fd70
makinosp <makinosp@icloud.com>
2024-09-10 22:04:14 +0900

    feat: split login and otp views, add validation to forms

---
+++ harmonie/Views/Authentication/LoginView.swift
@@ -0,0 +1,105 @@
+//
+//  LoginView.swift
+//  Harmonie
+//
+//  Created by makinosp on 2024/08/31.
+//
+
+import AsyncSwiftUI
+import VRCKit
+
+struct LoginView: View, AuthenticationServicePresentable {
+    @AppStorage(Constants.Keys.isSavedOnKeyChain) private var isSavedOnKeyChain = false
+    @AppStorage(Constants.Keys.username) private var username: String = ""
+    @Environment(AppViewModel.self) var appVM: AppViewModel
+    @State private var verifyType: VerifyType?
+    @State private var password: String = ""
+    @State private var isRequesting = false
+    @State private var isPresentedPopover = false
+
+    var body: some View {
+        NavigationStack {
+            VStack(spacing: 16) {
+                loginFields
+                enterButton
+            }
+            .padding(32)
+            .ignoresSafeArea(.keyboard)
+            .navigationDestination(item: $verifyType) { verifyType in
+                OtpView(verifyType: verifyType)
+            }
+        }
+        .onAppear {
+            if isSavedOnKeyChain,
+               let password = KeychainUtil.shared.getPassword(for: username) {
+                self.password = password
+            }
+        }
+    }
+
+    private var loginFields: some View {
+        VStack(spacing: 8) {
+            TextField("UserName", text: $username)
+                .textInputAutocapitalization(.never)
+                .textFieldStyle(.roundedBorder)
+            SecureField("Password", text: $password)
+                .textFieldStyle(.roundedBorder)
+            Toggle(isOn: $isSavedOnKeyChain) {
+                HStack {
+                    Label {
+                        Text("Store in Keychain")
+                    } icon: {
+                        Image(systemName: "key.icloud")
+                    }
+                    Button {
+                        isPresentedPopover.toggle()
+                    } label: {
+                        Image(systemName: "questionmark.circle")
+                    }
+                    .popover(isPresented: $isPresentedPopover) {
+                        Text(Constants.Messages.helpWithStoringKeychain)
+                            .padding()
+                            .presentationDetents([.fraction(1/4)])
+                    }
+                }
+                .font(.callout)
+                .foregroundStyle(Color(uiColor: .systemGray))
+            }
+        }
+        .padding(.horizontal, 8)
+    }
+
+    private var enterButton: some View {
+        AsyncButton {
+            await loginAction()
+        } label: {
+            if isRequesting {
+                ProgressView()
+            } else {
+                Text("Enter")
+            }
+        }
+        .buttonStyle(.bordered)
+        .buttonBorderShape(.capsule)
+        .disabled(isDisabledEnterButton)
+    }
+
+    private func loginAction() async {
+        defer { isRequesting = false }
+        isRequesting = true
+        verifyType = await appVM.login(
+            username: username,
+            password: password,
+            isSavedOnKeyChain: isSavedOnKeyChain
+        )
+    }
+
+    private var isDisabledEnterButton: Bool {
+        isRequesting || username.count < 4 || password.count < 8
+    }
+}
+
+#Preview {
+    LoginView()
+        .environment(AppViewModel())
+}
