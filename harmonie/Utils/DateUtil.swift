e9fe36925b641fc443912bd24ea5d823e9373ae3
makinosp <makinosp@icloud.com>
2024-09-15 11:31:05 +0900

    refact: refine relative datetime formatter

--- harmonie/Utils/DateUtil.swift
+++ harmonie/Utils/DateUtil.swift
@@ -8,13 +8,20 @@
 import Foundation
 
 final class DateUtil {
+    static let shared = DateUtil()
+    private let relativeDateTimeFormatter: RelativeDateTimeFormatter
+    private let comparedComponents: Set<Calendar.Component>
+
+    private init() {
+        relativeDateTimeFormatter = RelativeDateTimeFormatter()
+        comparedComponents = [.day, .year, .month, .minute, .second]
+    }
+
+    private func relativeDateComponents(from: Date = Date(), to: Date) -> DateComponents {
+        Calendar.current.dateComponents(comparedComponents, from: from, to: to)
+    }
+
     func formatRelative(from date: Date) -> String {
-        let formatter = RelativeDateTimeFormatter()
-        let components = Calendar.current.dateComponents(
-            [.day, .year, .month, .minute, .second],
-            from: Date(),
-            to: date
-        )
-        return formatter.localizedString(from: components)
+        relativeDateTimeFormatter.localizedString(from: relativeDateComponents(to: Date()))
     }
 }
