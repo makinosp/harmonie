e44df65353546b6730da955924b6fa812dfa1501
makinosp <makinosp@icloud.com>
2024-09-15 11:20:59 +0900

    refact: remove legacy dateformatter

--- harmonie/Utils/DateUtil.swift
+++ harmonie/Utils/DateUtil.swift
@@ -8,35 +8,6 @@
 import Foundation
 
 final class DateUtil {
-    static let shared = DateUtil()
-
-    private let yyyyMMddDateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "yyyy/MM/dd"
-        return dateFormatter
-    }()
-
-    private let HHmmDateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.dateFormat = "HH:mm"
-        return dateFormatter
-    }()
-
-    func formattedDateTime(from date: Date) -> String {
-        [formatToyyyyMMdd(from: date), formatToHHmm(from: date)]
-            .joined(separator: " ")
-    }
-
-    func formatToyyyyMMdd(from date: Date) -> String {
-        return yyyyMMddDateFormatter.string(from: date)
-    }
-
-    func formatToHHmm(from date: Date) -> String {
-        return HHmmDateFormatter.string(from: date)
-    }
-
     func formatRelative(from date: Date) -> String {
         let formatter = RelativeDateTimeFormatter()
         let components = Calendar.current.dateComponents(
