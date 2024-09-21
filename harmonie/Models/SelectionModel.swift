b9c67fce9f79d84fd8f4685638ab34a694def46c
makinosp <makinosp@icloud.com>
2024-09-21 23:20:20 +0900

    refact: refine model for views

---
+++ harmonie/Models/SelectionModel.swift
@@ -0,0 +1,19 @@
+//
+//  SelectionModel.swift
+//  Harmonie
+//
+//  Created by makinosp on 2024/07/28.
+//
+
+struct Selected: Hashable, Identifiable {
+    let id: String
+}
+
+struct SegmentIdSelection: Hashable, Sendable {
+    let selected: Selected
+    let segment: Segment
+}
+
+extension SegmentIdSelection: Identifiable {
+    var id: Int { hashValue }
+}
