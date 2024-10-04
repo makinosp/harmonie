97130fdb3cfaa9b687eb33907183bc74bda6e16e
makinosp <makinosp@icloud.com>
2024-10-04 21:10:18 +0900

    feat: add screen transition to FavoriteGroupsEditView in SettingView

---
+++ harmonie/Views/Setting/FavoriteGroups/FavoriteGroupsEditView.swift
@@ -0,0 +1,16 @@
+//
+//  FavoriteGroupsEditView.swift
+//  Harmonie
+//
+//  Created by makinosp on 2024/10/04.
+//
+
+import SwiftUI
+
+struct FavoriteGroupsEditView: View {
+    var body: some View {
+        Text("FavoriteGroupsEditView")
+            .navigationTitle("Edit favorite groups")
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
