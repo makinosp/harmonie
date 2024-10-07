52ab1b507b420eaafa1e7c2d0200b828c156008f
makinosp <makinosp@icloud.com>
2024-10-07 22:56:01 +0900

    feat: get app build from bundle info

--- harmonie/Utils/BundleUtil.swift
+++ harmonie/Utils/BundleUtil.swift
@@ -8,11 +8,19 @@
 import Foundation
 
 enum BundleUtil {
+    private static func getInfo(key: String) -> String {
+        Bundle.main.object(forInfoDictionaryKey: key) as? String ?? ""
+    }
+
     static var appName: String {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String ?? ""
     }
 
     static var appVersion: String {
         Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? ""
+    }
+
+    static var appBuild: String {
+        getInfo(key: "CFBundleVersion")
     }
 }
