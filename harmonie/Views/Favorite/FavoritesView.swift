d7fb26b3f3184b6128dd971df26ad00ce664b22e
makinosp <makinosp@icloud.com>
2024-10-13 21:49:54 +0900

    feat: refine FavoriteView, FavoriteViewModel

--- harmonie/Views/Favorite/FavoritesView.swift
+++ harmonie/Views/Favorite/FavoritesView.swift
@@ -10,7 +10,7 @@ import VRCKit
 
 struct FavoritesView: View {
     @Environment(FavoriteViewModel.self) var favoriteVM
-    @State private var selected: Selected?
+    @State private var selected: SegmentIdSelection?
     @State private var columnVisibility: NavigationSplitViewVisibility = .all
 
     var body: some View {
@@ -24,97 +24,67 @@ struct FavoritesView: View {
                     favoriteWorlds
                 }
             }
+            .overlay {
+                if favoriteVM.isSelectedEmpty {
+                    ContentUnavailableView {
+                        Label("No Favorites", systemImage: IconSet.favorite.systemName)
+                    }
+                    .background(Color(.systemGroupedBackground))
+                }
+            }
             .contentMargins(.top, 8)
             .navigationTitle(favoriteVM.segment.description)
             .navigationBarTitleDisplayMode(.inline)
-            .toolbarTitleMenu {
-                Picker("", selection: $favoriteVM.segment) {
-                    ForEach(FavoriteViewSegment.allCases) { segment in
-                        Label {
-                            Text(segment.description)
-                        } icon: {
-                            segment.icon
-                        }
-                        .tag(segment)
-                    }
-                }
-            }
-        } detail: {
-//            if let selected = selected {
-//                switch favoriteVM.segment {
-//                case .friends:
-//                    UserDetailPresentationView(id: selected.id)
-//                case .world:
-//                    WorldPresentationView(id: selected.id)
-//                }
-//            } else {
-//                ContentUnavailableView {
-//                    Label {
-//                        Text("Select an item")
-//                    } icon: {
-//                        IconSet.favorite.icon
-//                    }
-//                }
-//            }
-        }
+            .toolbarTitleMenu { toolbarTitleMenu }
+        } detail: { detail }
         .navigationSplitViewStyle(.balanced)
     }
 
-    @ViewBuilder
+    @ViewBuilder private var toolbarTitleMenu: some View {
+        @Bindable var favoriteVM = favoriteVM
+        Picker("", selection: $favoriteVM.segment) {
+            ForEach(FavoriteViewSegment.allCases) { segment in
+                Label {
+                    Text(segment.description)
+                } icon: {
+                    segment.icon
+                }
+                .tag(segment)
+            }
+        }
+    }
+
+    @ViewBuilder private var detail: some View {
+        if let selectedContainer = selected {
+            switch selectedContainer.segment {
+            case .friends:
+                UserDetailPresentationView(id: selectedContainer.selected.id)
+            case .world:
+                WorldPresentationView(id: selectedContainer.selected.id)
+            default:
+                EmptyView()
+            }
+        } else {
+            ContentUnavailableView {
+                Label {
+                    Text("Select an item")
+                } icon: {
+                    IconSet.favorite.icon
+                }
+            }
+        }
+    }
+
     var favoriteFriends: some View {
-        let groups = favoriteVM.favoriteGroups(.friend)
-        ForEach(groups) { group in
-            if let friends = favoriteVM.getFavoriteFriends(group.id) {
-                friendsDisclosureGroup(group.displayName, friends: friends)
-            } else {
-                groupLabel(group.displayName, count: .zero, max: .friends)
-            }
-        }
-        .overlay {
-            if groups.isEmpty {
-                ContentUnavailableView {
-                    Label {
-                        Text("No Favorites")
-                    } icon: {
-                        IconSet.favorite.icon
-                    }
+        Section("Friends") {
+            let groups = favoriteVM.favoriteGroups(.friend)
+            ForEach(groups) { group in
+                if let friends = favoriteVM.getFavoriteFriends(group.id) {
+                    friendsDisclosureGroup(group.displayName, friends: friends)
+                } else {
+                    groupLabel(group.displayName, count: .zero, max: .friends)
                 }
-                .background(Color(.systemGroupedBackground))
             }
-        }
-    }
-
-    var favoriteWorlds: some View {
-        ForEach(favoriteVM.favoriteWorldGroups) { favoriteWorlds in
-            if let group = favoriteWorlds.group {
-                worldDisclosureGroup(group.displayName, favoriteWorlds: favoriteWorlds)
-            }
-        }
-        .overlay {
-            if favoriteVM.favoriteWorlds.isEmpty {
-                ContentUnavailableView {
-                    Label {
-                        Text("No Favorites")
-                    } icon: {
-                        IconSet.favorite.icon
-                    }
-                }
-                .background(Color(.systemGroupedBackground))
-            }
-        }
-    }
-
-    private func worldDisclosureGroup(
-        _ title: any StringProtocol,
-        favoriteWorlds: FavoriteWorldGroup
-    ) -> DisclosureGroup<some View, some View> {
-        DisclosureGroup {
-            ForEach(favoriteWorlds.worlds) { world in
-                worldItem(world)
-                    .tag(Selected(id: world.id))
-            }
-        } label: {
-            groupLabel(title, count: favoriteWorlds.worlds.count, max: .world)
         }
     }
 
@@ -135,6 +105,30 @@ struct FavoritesView: View {
             }
         } label: {
             groupLabel(title, count: friends.count, max: .friends)
+        }
+    }
+
+    var favoriteWorlds: some View {
+        Section("World") {
+            ForEach(favoriteVM.favoriteWorldGroups) { favoriteWorlds in
+                if let group = favoriteWorlds.group {
+                    worldDisclosureGroup(group.displayName, favoriteWorlds: favoriteWorlds)
+                }
+            }
+        }
+    }
+
+    private func worldDisclosureGroup(
+        _ title: any StringProtocol,
+        favoriteWorlds: FavoriteWorldGroup
+    ) -> DisclosureGroup<some View, some View> {
+        DisclosureGroup {
+            ForEach(favoriteWorlds.worlds) { world in
+                worldItem(world)
+                    .tag(Selected(id: world.id))
+            }
+        } label: {
+            groupLabel(title, count: favoriteWorlds.worlds.count, max: .world)
         }
     }
 
