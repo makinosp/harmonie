80820fef5634624de2baa185d68e62527d8811de
makinosp <makinosp@icloud.com>
2024-10-19 16:11:50 +0900

    refactor: improvement message model

--- harmonie/Utils/Constants.swift
+++ harmonie/Utils/Constants.swift
@@ -34,18 +34,33 @@ enum Constants {
         case helpWithStoringAuthenticationTokens
         case helpWithStoringPassword
         case helpWithCommunicationSecurity
+    }
+}
 
-        var text: String {
-            switch self {
-            case .helpWithVRChatAPIAuthencication:
-                String(localized: "msg_help_with_vrchat_api_authentication", table: "Message")
-            case .helpWithStoringAuthenticationTokens:
-                String(localized: "msg_help_with_storing_authentication_tokens", table: "Message")
-            case .helpWithStoringPassword:
-                String(localized: "msg_help_with_storing_password", table: "Message")
-            case .helpWithCommunicationSecurity:
-                String(localized: "msg_help_with_communication_security", table: "Message")
-            }
+extension Constants.Messages {
+    var title: String {
+        switch self {
+        case .helpWithVRChatAPIAuthencication:
+            String(localized: "About the authentication method of VRChatAPI", table: "Message")
+        case .helpWithStoringAuthenticationTokens:
+            String(localized: "Security in Authentication Token Retention", table: "Message")
+        case .helpWithStoringPassword:
+            String(localized: "Security in password storage", table: "Message")
+        case .helpWithCommunicationSecurity:
+            String(localized: "Communication security", table: "Message")
+        }
+    }
+
+    var text: String {
+        switch self {
+        case .helpWithVRChatAPIAuthencication:
+            String(localized: "msg_help_with_vrchat_api_authentication", table: "Message")
+        case .helpWithStoringAuthenticationTokens:
+            String(localized: "msg_help_with_storing_authentication_tokens", table: "Message")
+        case .helpWithStoringPassword:
+            String(localized: "msg_help_with_storing_password", table: "Message")
+        case .helpWithCommunicationSecurity:
+            String(localized: "msg_help_with_communication_security", table: "Message")
         }
     }
 }
