be3b66e2f41d867fa07089788c1eca6f46e227a0
makinosp <makinosp@icloud.com>
2024-10-20 22:10:44 +0900

    feat: add preview string enum

---
+++ harmonie/Previews/PreviewString.swift
@@ -0,0 +1,14 @@
+//
+//  PreviewString.swift
+//  Harmonie
+//
+//  Created by makinosp on 2024/10/20.
+//
+
+enum PreviewString {
+    enum name: String {
+        case emma
+        case josh
+        case clarke
+    }
+}
