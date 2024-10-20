d1b0c67d62e7dc9b5b23fea6d245319a06e276ea
makinosp <makinosp@icloud.com>
2024-10-20 13:55:52 +0900

    refact: code refactoring

--- harmonie/Extensions/Binding+NilCoalescing.swift
+++ harmonie/Extensions/Binding+NilCoalescing.swift
@@ -7,9 +7,9 @@
 
 import SwiftUICore
 
-extension Binding {
-    static func ?? <T>(optional: Self, defaultValue: T) -> Binding<T> where T: Sendable, Value == T? {
-        .init(
+extension Binding where Value: Sendable {
+    static func ?? <T>(optional: Self, defaultValue: T) -> Binding<T> where Value == T? {
+        Binding<T>(
             get: { optional.wrappedValue ?? defaultValue },
             set: { optional.wrappedValue = $0 }
         )
