303f0a994f6349efb103bdc7a04fe715236ed670
makinosp <makinosp@icloud.com>
2024-10-31 23:34:16 +0900

    feat: improvement errors, error handler

--- harmonie/Modifiers/View+ErrorAlertModifier.swift
+++ harmonie/Modifiers/View+ErrorAlertModifier.swift
@@ -14,10 +14,22 @@ extension View {
     }
 }
 
-@MemberwiseInit
+@MainActor @MemberwiseInit
 private struct ErrorAlertModifier {
     @Environment(AppViewModel.self) var appVM: AppViewModel
     @Init(.internal, escaping: true, label: "_") private let action: () -> Void
+
+    typealias ErrorKeyPath<E> = ReferenceWritableKeyPath<AppViewModel, E?>
+
+    func isThrownError<E>(_ errorKeyPath: ErrorKeyPath<E>) -> Binding<Bool> where E: Error {
+        Binding<Bool>(
+            get: { appVM[keyPath: errorKeyPath] != nil },
+            set: { isErrorActive in
+                guard !isErrorActive else { return }
+                appVM[keyPath: errorKeyPath] = nil
+            }
+        )
+    }
 }
 
 extension ErrorAlertModifier: ViewModifier {
@@ -25,12 +37,21 @@ extension ErrorAlertModifier: ViewModifier {
         @Bindable var appVM = appVM
         content
             .alert(
-                isPresented: $appVM.isPresentedAlert,
+                isPresented: isThrownError(\.vrckError),
                 error: appVM.vrckError
             ) { _ in
                 Button("OK") {
                     action()
-                    appVM.vrckError = nil
+                }
+            } message: { error in
+                Text(error.failureReason ?? "Try again later.")
+            }
+            .alert(
+                isPresented: isThrownError(\.applicationError),
+                error: appVM.applicationError
+            ) { _ in
+                Button("OK") {
+                    action()
                 }
             } message: { error in
                 Text(error.failureReason ?? "Try again later.")
