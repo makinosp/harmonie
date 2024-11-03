3c48ce9ef74166e64f2414464f00a17acbc3571a
makinosp <makinosp@icloud.com>
2024-11-03 21:01:02 +0900

    feat: improvement preview data for FavoritePreviewService

--- harmonie/Previews/Services/FavoritePreviewService.swift
+++ harmonie/Previews/Services/FavoritePreviewService.swift
@@ -13,38 +13,47 @@ import VRCKit
 final actor FavoritePreviewService: APIService, FavoriteServiceProtocol {
     let client: APIClient
 
-    func listFavoriteGroups() async throws -> [FavoriteGroup] {
-        FavoriteType.allCases.flatMap { type in
-            (1...3).map { index in
-                FavoriteGroup(
-                    id: "fvgrp_\(UUID().uuidString)",
-                    displayName: "\(type.rawValue.capitalized) Group \(index)",
-                    name: "group_\(index)",
-                    ownerId: PreviewData.shared.previewUser.id,
-                    tags: [],
-                    type: type,
-                    visibility: .private
-                )
-            }
+    let favoriteGroups = FavoriteType.allCases.flatMap { type in
+        (1...3).map { number in
+            FavoriteGroup(
+                id: "fvgrp_\(UUID().uuidString)",
+                displayName: "\(type.rawValue.capitalized) Group \(number)",
+                name: "group_\(number)",
+                ownerId: PreviewData.shared.previewUser.id,
+                tags: [],
+                type: type,
+                visibility: .private
+            )
         }
+    }
+
+    func listFavoriteGroups() async throws -> [FavoriteGroup] {
+        favoriteGroups
     }
 
     func listFavorites(type: FavoriteType) async throws -> [Favorite] {
-        switch type {
-        case .friend:
-            PreviewData.shared.onlineFriends.prefix(5).map { friend in
-                Favorite(id: UUID().uuidString, favoriteId: friend.id, tags: ["group_1"], type: .friend)
-            }
-        default: []
-        }
+        // No implementation required
+        []
     }
 
     func listFavorites(n: Int, offset: Int, type: FavoriteType, tag: String?) async throws -> [Favorite] {
+        // No implementation required
         []
     }
 
     func fetchFavoriteList(favoriteGroups: [FavoriteGroup], type: FavoriteType) async throws -> [FavoriteList] {
-        []
+        favoriteGroups.enumerated().map { (index, group) in
+            switch (index, group.type) {
+            case (6, .friend):
+                FavoriteList(
+                    id: group.id,
+                    favorites: PreviewData.shared.onlineFriends.prefix(5).map { friend in
+                        Favorite(favoriteId: friend.id, type: group.type)
+                    }
+                )
+            default: FavoriteList(id: group.id, favorites: [])
+            }
+        }
     }
 
     public func addFavorite(
@@ -52,16 +61,24 @@ final actor FavoritePreviewService: APIService, FavoriteServiceProtocol {
         favoriteId: String,
         tag: String
     ) async throws -> Favorite {
-        Favorite(id: UUID().uuidString, favoriteId: favoriteId, tags: [tag], type: type)
+        Favorite(favoriteId: favoriteId, tags: [tag], type: type)
     }
 
     func updateFavoriteGroup(
         source: FavoriteGroup,
         displayName: String,
         visibility: FavoriteGroup.Visibility
-    ) async throws {}
+    ) async throws {
+        // No implementation required
+    }
 
     func removeFavorite(favoriteId: String) async throws -> SuccessResponse {
         SuccessResponse(success: .ok)
+    }
+}
+
+private extension Favorite {
+    init(favoriteId: String, tags: [String] = [], type: FavoriteType) {
+        self.init(id: UUID().uuidString, favoriteId: favoriteId, tags: tags, type: type)
     }
 }
