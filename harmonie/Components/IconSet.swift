b5515d2b5a0650a48967542566206a5f463b5892
makinosp <makinosp@icloud.com>
2024-11-04 21:35:32 +0900

    feat: change icon of available platform

--- harmonie/Components/IconSet.swift
+++ harmonie/Components/IconSet.swift
@@ -72,6 +72,9 @@ enum IconSet {
     static var logout: some Iconizable {
         Icon("rectangle.portrait.and.arrow.forward")
     }
+    static var platform: some Iconizable {
+        Icon("square.stack.3d.up")
+    }
     static var plusCircleFilled: some Iconizable {
         Icon("plus.circle.fill")
     }
