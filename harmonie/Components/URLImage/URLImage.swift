bf6c9fa23cd99a1174b63a5cb1bd4addcedbd11d
makinosp <makinosp@icloud.com>
2024-11-11 16:50:41 +0900

    feat: implement commonized URLImage view

---
+++ harmonie/Components/URLImage/URLImage.swift
@@ -0,0 +1,55 @@
+//
+//  URLImage.swift
+//  Harmonie
+//
+//  Created by makinosp on 2024/11/11.
+//
+
+import MemberwiseInit
+import NukeUI
+import SwiftUI
+
+@MemberwiseInit
+struct URLImage<S>: View where S: Shape {
+    @State private var isImageLoaded = false
+    @Init(.internal) private let imageUrl: URL?
+    @Init(.internal, default: nil) private let thumbnailImageUrl: URL?
+    @Init(.internal) private let shape: S
+    @Init(.internal) private let size: CGSize
+    private let color = Color(.systemFill)
+
+    var body: some View {
+        lazyImage(url: imageUrl) {
+            lazyImage(url: thumbnailImageUrl) {
+                placeholder
+            }
+        }
+    }
+
+    private func lazyImage(url: URL?) -> some View {
+        LazyImage(url: url) { state in
+            if let image = state.image {
+                image
+                    .resizable()
+                    .scaledToFill()
+            } else if url != nil && state.error != nil {
+                placeholder
+                    .overlay(IconSet.photo.icon)
+            } else {
+                placeholder
+            }
+        }
+        .onCompletion { _ in
+            isImageLoaded = true
+        }
+        .animation(.default, value: isImageLoaded)
+        .frame(size: size)
+        .clipShape(shape)
+    }
+
+    private var placeholder: some View {
+        shape
+            .fill(color)
+            .frame(size: size)
+    }
+}
